cbb1dfae8abe069e827804d4ff2d4b349b805361
nirmal-vaja <nirmalvaja123@gmail.com>
2023-03-07 01:56:38 -0500

    Routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -7,10 +7,10 @@ end
 
 Rails.application.routes.draw do
   constraints SubdomainConstraint do
-    resources :projects
+    devise_for :users
   end
 
-  devise_for :users
+  # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
