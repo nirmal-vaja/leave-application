62ea6a1d461b0643de490338143029fa58ee8766
nirmal-vaja <nirmalvaja123@gmail.com>
2023-03-09 01:12:42 -0500

    WIP

--- config/routes/api.rb
+++ config/routes/api.rb
@@ -1,10 +1,8 @@
 # frozen_string_liberal: true
 
 namespace :api do
-  namespace :v1 do
-    scope :users, module: :users do
-      post '/', to: 'registrations#create', as: :user_registration
-    end
+  scope :users, module: :users do
+    post '/', to: 'registrations#create', as: :user_registration
   end
 end
 
