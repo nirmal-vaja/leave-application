d96a2509827b541594cf3b9f00ac47fb27e68e94
nirmal-vaja <nirmalvaja123@gmail.com>
2023-03-10 03:49:52 -0500

    Fix

--- config/routes.rb
+++ config/routes.rb
@@ -8,11 +8,11 @@ end
 Rails.application.routes.draw do
   constraints SubdomainConstraint do
     devise_for :users
+    root "home#index"
   end
 
   # devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  root "home#index"
 end
