be252778e2708998d89180c87970d5ec0d67551b
nirmal-vaja <nirmalvaja123@gmail.com>
2023-04-10 18:13:07 +0530

    wip

--- config/initializers/apartment.rb
+++ config/initializers/apartment.rb
@@ -114,4 +114,4 @@ end
 Rails.application.config.middleware.use Apartment::Elevators::Subdomain
 # Rails.application.config.middleware.use Apartment::Elevators::FirstSubdomain
 # Rails.application.config.middleware.use Apartment::Elevators::Host
-Apartment::Elevators::Subdomain.excluded_subdomains = ['www', 'ec2-65-1-3-48.ap-south-1.compute']
+Apartment::Elevators::Subdomain.excluded_subdomains = ['www', 'ec2-65-1-3-48.ap-south-1.compute', 'ec2-65-1-3-48']
